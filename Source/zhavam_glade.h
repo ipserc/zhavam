b11f521d8005eef2a94eb4f6362792df7f5aa836
ipserc <ipserc@users.noreply.github.com>
2018-03-11 21:45:05 +0100

    gladec.sh included
    Creates the .c and .h files with the code generated from glade.

--- Source/zhavam_glade.h
+++ Source/zhavam_glade.h
@@ -1,8 +1,9 @@
 /*
  * zhavam_glade.h
  *
- *  Created on: 4 mar. 2018
+ *  Created on: dom mar 11 21:29:27 CET 2018
  *      Author: ipserc
+ *		File and code generated automatically from ./gladec.sh
  */
 
 #ifndef SOURCE_ZHAVAM_GLADE_H_
